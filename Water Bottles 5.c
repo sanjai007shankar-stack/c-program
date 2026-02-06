8f19a935dce8bf56f47078b40ae812dd46475d36
sanjai007shankar-stack <sanjai007shankar@gmail.com>
2026-02-06 14:45:28 +0530

    Create  Water Bottles 5.c

---
+++ Water Bottles 5.c
@@ -0,0 +1,9 @@
+int total=numBottles;
+    int empty=numBottles;
+    while(empty>=numExchange){
+        int newBottle = empty/numExchange;
+        total=total+newBottle;
+        empty=newBottle+(empty%numExchange);
+    }
+    return total;
+    
