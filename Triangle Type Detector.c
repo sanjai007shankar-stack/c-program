346cc8000848ccb73fc5d04893efc4514cbe3564
sanjai007shankar-stack <sanjai007shankar@gmail.com>
2026-02-06 14:32:46 +0530

    Create Triangle Type Detector.c

---
+++ Triangle Type Detector.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a + b > c &&  b + c > a && c + a > b){
+        if(a==b && a==c && b==c){
+        printf("Equilateral Triangle");
+        }else if(a==b || b==c || c==a){
+        printf("Exactly two sides are equal");
+        }else{
+            printf("Scalene Triangle");
+}
+}else 
+    {
+        printf("Not a valid triangle");
+    }
+    return 0;
+}
